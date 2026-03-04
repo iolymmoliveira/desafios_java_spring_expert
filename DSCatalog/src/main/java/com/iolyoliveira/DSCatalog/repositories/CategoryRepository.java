6e046a54f1edb37923caf65d6a51813c74a45ce8
ioly Oliveira <iolymmoliveira@gmail.com>
2026-03-04 15:12:31 -0300

    feat: CategoryRepository

---
+++ DSCatalog/src/main/java/com/iolyoliveira/DSCatalog/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.iolyoliveira.DSCatalog.repositories;
+
+import com.iolyoliveira.DSCatalog.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
