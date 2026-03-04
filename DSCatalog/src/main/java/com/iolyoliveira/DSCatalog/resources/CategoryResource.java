dcc53fcb8dbe5b0c3f91579d0fb8e717bdd6786d
ioly Oliveira <iolymmoliveira@gmail.com>
2026-03-04 15:42:18 -0300

    feat: add CategoryResource with findAll and findById endpoints

---
+++ DSCatalog/src/main/java/com/iolyoliveira/DSCatalog/resources/CategoryResource.java
@@ -0,0 +1,32 @@
+package com.iolyoliveira.DSCatalog.resources;
+
+import com.iolyoliveira.DSCatalog.dto.CategoryDTO;
+import com.iolyoliveira.DSCatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO result = service.findById(id);
+        return ResponseEntity.ok().body(result);
+    }
+}
