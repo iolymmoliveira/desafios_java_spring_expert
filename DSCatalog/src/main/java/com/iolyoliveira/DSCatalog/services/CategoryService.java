9370f7a873486d52ffc8f6b575e235f8469c604d
ioly Oliveira <iolymmoliveira@gmail.com>
2026-03-05 11:31:55 -0300

    feat: implement DELETE Category

--- DSCatalog/src/main/java/com/iolyoliveira/DSCatalog/services/CategoryService.java
+++ DSCatalog/src/main/java/com/iolyoliveira/DSCatalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.iolyoliveira.DSCatalog.services;
 import com.iolyoliveira.DSCatalog.dto.CategoryDTO;
 import com.iolyoliveira.DSCatalog.entities.Category;
 import com.iolyoliveira.DSCatalog.repositories.CategoryRepository;
+import com.iolyoliveira.DSCatalog.services.exceptions.DatabaseException;
 import com.iolyoliveira.DSCatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -47,6 +50,19 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+
+        try{
+            repository.deleteById(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DatabaseException("Referential integrity failure");
         }
     }
 }
---
+++ DSCatalog/src/main/java/com/iolyoliveira/DSCatalog/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.iolyoliveira.DSCatalog.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
